db29713e13a167626d9b7272dea03c65cacadbd6
Faiyaz Ahmed <faiyaz1@ualberta.ca>
2021-08-01 20:27:37 -0600

    Added Snow mountain scene

---
+++ natHACKS/Assets/Nature/MyScripts/gameStartClass.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class gameStartClass  
+{
+    public static bool gameStart = false;
+    public static bool ml = false;
+    public static bool bp = false;
+}
